9535a623a1ac4ff802e39fa83f8ffe2d91753a3e
Anderson Alencar <joveransu@gmail.com>
2022-09-25 22:05:24 -0300

    Update choose-words.js

--- scripts/choose-words.js
+++ scripts/choose-words.js
@@ -6,11 +6,21 @@ function selectWord() {
 }
 
 function nextBox(elemento){
-    elemento.value.toUpperCase()
     var id = elemento.id
-    var newFocus = 'letter' + Number(elemento.id) + 1
-    document.getElementById(newFocus).focus()
-    console.log('Sei lá: ' + elemento.value + ', ' + elemento.id)
+    var n = Number(id[6]) + Number(1)
+    if(n < 7){
+        var newFocus = 'letter' + n
+    } else {
+        var newFocus = 'letter6'
+    }
+    elemento.value = 'a'.toUpperCase()
+    console.log(event.keyCode)
+    //console.log(`Sei lá: ${newFocus}, ${elemento.value}, ${elemento.id}`)
+    var nextBox = document.getElementById(newFocus)
+
+    nextBox.focus()
+    nextBox.value = ' '
+    //setTimeout(changeBox(elemento, newFocus), 250)
 }
 
 function selectedBox(elemento){
