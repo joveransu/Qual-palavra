b46caf3d1fd3a9add14da1c9fea61a640b86be88
Anderson Alencar <joveransu@gmail.com>
2022-10-03 11:37:14 -0300

    feedback 85% feito

--- scripts/alerts.js
+++ scripts/alerts.js
@@ -1,3 +1,7 @@
 function alertBlocked(){
     window.alert('Ops... Ainda estamos desenvolvimento, aguarde atualizações!')
 }
+
+function alertRegister(){
+    window.alert('Recurso em desenvolvimento, mas você pode deixar seu feedback por enquanto.')
+}
--- scripts/feedbacks.js
+++ scripts/feedbacks.js
@@ -5,6 +5,23 @@ function registerFeedBack(){
     let feedback = document.getElementById('feedback')
     if (feedback.value.length < 10) return alert('Preencha o campo do FeedBack')
 
+    let mostrar = document.getElementById('mostrar')
+    if (mostrar.checked){
+        let zone_feed = document.querySelector('section#feeds')
+        let newZone = document.createElement('div')
+        let newTitle = document.createElement('h2')
+        let newFeed = document.createElement('p')
+
+        newTitle.innerHTML = `${name.value}`
+        newZone.appendChild(newTitle)
+
+        newFeed.innerHTML = `${feedback.value}`
+        newZone.appendChild(newFeed)
+
+        zone_feed.appendChild(newZone)
+    }
+
+
     alert('Seu FeedBack foi enviado, muito obrigado :)')
-    document.location.href = '../index.html'
+    //document.location.href = '../index.html'
 }
